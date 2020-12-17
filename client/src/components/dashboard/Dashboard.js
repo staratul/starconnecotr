d8972ca79f832ebd3df1437a704a17337fb53945
Atul Chauhan <atulji.star@gmail.com>
2020-12-17 15:03:42 +0530

    add education and add experience

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import propTypes from 'prop-types';
 import Spinner from '../layout/Spinner';
+import DashboardActions from './DashboardActions';
 import { getCurrentProfile } from '../../actions/profile';
 
 const Dashboard = ({ getCurrentProfile, auth: {user}, profile: {profile, loading} }) => {
@@ -15,7 +16,9 @@ const Dashboard = ({ getCurrentProfile, auth: {user}, profile: {profile, loading
         <p className="lead">
         <i className="fa fa-user" aria-hidden="true"></i> Welcome { user && user.name }
         </p>
-        {profile !== null ? <Fragment>has</Fragment> : <Fragment>
+        {profile !== null ? <Fragment>
+                <DashboardActions />
+            </Fragment> : <Fragment>
             <p>You have not yet setup a profile, please add some info</p>    
             <Link to="/create-profile" className="btn btn-primary my-1">
                 Create Profile
